76ca2268a7dd03981b0e01ff215cc0e060263a33
Matheus Dos Santos <matheus2050@gmail.com>
2024-05-31 23:01:36 -0300

    Adicionado todos os metodos

--- app.ts
+++ app.ts
@@ -1,9 +1,19 @@
 import { CompanyAccount } from './class/CompanyAccount'
 import { PeopleAccount } from './class/PeopleAccount'
+import { TsAccount } from './class/TsAccount'
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
+peopleAccount.deposit(20)
 console.log(peopleAccount)
-peopleAccount.deposit()
+peopleAccount.withdraw(10)
+console.log(peopleAccount)
+
 const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
-companyAccount.deposit()
+companyAccount.deposit(20)
 console.log(companyAccount)
+companyAccount.getLoan(25)
+console.log(companyAccount)
+
+const tsAccount: TsAccount = new TsAccount('Alexandre', 30)
+tsAccount.setBalance(600)
+console.log(tsAccount)
